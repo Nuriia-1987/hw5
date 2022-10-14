88ca69960f59971eb51269d53dd6ccc0567ecf0d
NUriia-1987 <tatarochka-9@mail.ru>
2022-10-14 15:48:48 +0600

    hm-5

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,8 +1,9 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .serializers import *
 from .models import Director, Movie, Review
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 
 @api_view(['GET', 'POST'])
@@ -48,6 +49,7 @@ def director_item_view(request, id):
 
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def movie_view(request):
     if request.method == 'GET':
         movie = Movie.objects.all()
